3ab8329f18522c571d19c1c67d252af78634bad0
KrisN9 <kristinank992@gmail.com>
2022-10-21 19:34:43 +0000

    Iniciado con función for

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const pronoun = ["the", "our", "a"];
+const adj = ["big", "great", "awesome"];
+const noun = ["raccoon", "jogger", "tiger"];
+const extention = [".com", ".net", ".es", ".info"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extention.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extention[l]);
+      }
+    }
+  }
+}
