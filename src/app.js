70d9267e9964041edc3da02085adf47821714d0a
KrisN9 <kristinank992@gmail.com>
2022-10-26 13:49:12 +0000

    generando dominios con funcion usando for

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ window.onload = function() {
 const pronoun = ["the", "our", "a"];
 const adj = ["big", "great", "awesome"];
 const noun = ["raccoon", "jogger", "tiger"];
-const extention = [".com", ".net", ".es", ".info"];
+const extension = [".com", ".net", ".es", ".info"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < extention.length; l++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + extention[l]);
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
       }
     }
   }
